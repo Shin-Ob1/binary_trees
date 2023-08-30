c32abd3eb55ce466a6778ae3c2b4ceb1c18fa353
Obinna Ukwa <obikelv@gmail.com>
2023-08-30 05:37:03 -0700

    debug balance tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,33 @@
 #include "binary_trees.h"
 
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ *
+ * @tree: measures the height of a binary tree
+ * Return: tree height
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t lheight, rheight;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL)
+		lheight = 0;
+	else
+		lheight = binary_tree_height(tree->left) + 1;
+	if (tree->right == NULL)
+		rheight = 0;
+	else
+		rheight = binary_tree_height(tree->right) + 1;
+	if (lheight > rheight)
+		return (lheight);
+	return (rheight);
+}
+
 /**
  * binary_tree_balance - measures the balance factor of
  * a binary tree
@@ -10,18 +38,13 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int lheight, rheight;
+	size_t lheight, rheight;
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left == NULL)
-		lheight = 0;
-	else
-		lheight = binary_tree_balance(tree->left) + 1;
-	if (tree->right == NULL)
-		rheight = 0;
-	else
-		rheight = binary_tree_balance(tree->right) + 1;
+
+	lheight = binary_tree_height(tree->left);
+	rheight = binary_tree_height(tree->right);
 
 	return (lheight - rheight);
 }
