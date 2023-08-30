087c8d7dff1a34a25297123565d458aeed0a546b
Obinna Ukwa <obikelv@gmail.com>
2023-08-30 03:43:20 -0700

    debug insert left child

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -22,8 +22,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		newnode->left = parent->left;
 		parent->left->parent = newnode;
 	}
-	else
-		parent->left = newnode;
+	parent->left = newnode;
 
 	return (newnode);
 }
